e60831b976a911318e1c78c42745384835d02f90
王艺聪 <2572285750@qq.com>
2021-12-13 14:44:41 +0800

    Initial commit

---
+++ Zuul/src/main/java/com/southwind/ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableAutoConfiguration
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
